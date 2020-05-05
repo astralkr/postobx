626fc1ade2f9f0ba5c7e6c4bd280bfc9d7859001
AstralKR <eud28280@naver.com>
2020-05-05 21:01:36 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,6 @@ import os
 
 client = discord.Client()
 
-access_token = os.environ["BOT_TOKEN"]
-cleint.run('access_token')
 
 
 @client.event
@@ -12,17 +10,16 @@ async def on_ready():
     print('login')
     print(client.user.name)
     print(client.user.id)
-    print('--------------------')
-    await client.change_presence(game=discord.Game(name='Postbox' , type=1))
+    print('----')
+    game = discord.Game("우체국 시험중!")
+    await client.change_presence(status=discord.Status.online,activity=game)
 
 @client.event
 async def on_message(message):
-    if client.private_channels and message.author.id != "707085039959146516":
-        await client.get_channel("707089326781104179").send(message.author.name + "(" + str(message.author.id) + "): " + message.content)
+    if isinstance(message.channel, discord.abc.PrivateChannel) and message.author.id != 70708503995914651:
+        channel = client.get_channel(707089326781104179)
+        await channel.send(str(message.author.name) + "(" + str(message.author.id) + ") : " + str(message.content))
 
 
-    if message.content.startswith("/DM"):
-        author = message.guild.get_member(int(message.content[4:22]))
-        msg = message.content[23:]
-        await author.send(msg)
-
+access_token = os.environ["BOT_TOKEN"]
+client.run(access_token)
